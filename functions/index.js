f8dce225b58e6743cd8013f69b0bf3d5f20ca140
mikeyp957 <peraram2@gmail.com>
2021-07-31 17:46:52 -0700

    connect routes to index

--- functions/index.js
+++ functions/index.js
@@ -3,21 +3,24 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 const FBAuth = require("./util/fbAuth");
-const {getAllMessages, postOneMessage} = require("./handlers/messages");
-const {signup, login, uploadImage, addUserDetails} = require("./handlers/users");
+const {getAllMessages, postOneMessage, getOneMessage} = require("./handlers/messages");
+const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require("./handlers/users");
 
 
 // messages routes
 app.get("/messages", getAllMessages);
 // post one message
 app.post("/messages", FBAuth, postOneMessage);
-app.post('/user', FBAuth, addUserDetails)
+app.get('/messages/:messageId', getOneMessage)
+//Todo:
+//delete, like, unlike, comment on messages
 
 // user routes
 app.post("/signup", signup);
 app.post("/login", login);
 app.post('/user/image',FBAuth, uploadImage)
-
+app.post('/user', FBAuth, addUserDetails)
+app.get('/user', FBAuth, getAuthenticatedUser)
 
 //  https://baseurl.com/api/
 exports.api = functions.https.onRequest(app);
