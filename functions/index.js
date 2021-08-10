7108e8af8504ccae534c842eceafbd910bb9f0b4
mikeyp957 <peraram2@gmail.com>
2021-08-10 14:24:18 -0700

    add link/ unlike routes

--- functions/index.js
+++ functions/index.js
@@ -3,24 +3,33 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 const FBAuth = require("./util/fbAuth");
-const {getAllMessages, postOneMessage, getOneMessage} = require("./handlers/messages");
+const {getAllMessages, postOneMessage, getOneMessage, commentOnMessage, likeMessage, unlikeMessage} = require("./handlers/messages");
 const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require("./handlers/users");
 
 
 // messages routes
 app.get("/messages", getAllMessages);
-// post one message
+app.get('/messages/:messagesId', getOneMessage)
+// like
+app.get('/messages/:messageId/like', FBAuth, likeMessage)
+app.get('/messages/:messageId/unlike', FBAuth, unlikeMessage)
+// post messages
 app.post("/messages", FBAuth, postOneMessage);
-app.get('/messages/:messageId', getOneMessage)
-//Todo:
-//delete, like, unlike, comment on messages
+// comment on message
+app.post(`/messages/:messagesId/comment`, FBAuth, commentOnMessage)
+//delete
 
 // user routes
+app.get('/user', FBAuth, getAuthenticatedUser)
 app.post("/signup", signup);
 app.post("/login", login);
 app.post('/user/image',FBAuth, uploadImage)
 app.post('/user', FBAuth, addUserDetails)
-app.get('/user', FBAuth, getAuthenticatedUser)
+
+// activity routes
+    // training
+    // outdoor
+// 
 
 //  https://baseurl.com/api/
 exports.api = functions.https.onRequest(app);
