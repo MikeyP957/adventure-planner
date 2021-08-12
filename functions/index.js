f8c14890fbbd1d108408379835436bb1cb58dcbc
mikeyp957 <peraram2@gmail.com>
2021-08-11 23:25:54 -0700

    add firebase function triggers, user detail, refac

--- functions/index.js
+++ functions/index.js
@@ -17,6 +17,7 @@ const {
   login,
   uploadImage,
   addUserDetails,
+  getUserDetails,
   getAuthenticatedUser,
 } = require("./handlers/users");
 
@@ -24,18 +25,19 @@ const {db} = require("./util/admin");
 
 // messages routes
 app.get("/messages", getAllMessages);
-app.get("/messages/:messagesId", getOneMessage);
+app.get("/messages/:messageId", getOneMessage);
 // like
 app.get("/messages/:messageId/like", FBAuth, likeMessage);
 app.get("/messages/:messageId/unlike", FBAuth, unlikeMessage);
 // post messages
 app.post("/messages", FBAuth, postOneMessage);
 // comment on message
-app.post("/messages/:messagesId/comment", FBAuth, commentOnMessage);
+app.post("/messages/:messageId/comment", FBAuth, commentOnMessage);
 // delete
 app.delete("/messages/:messageId", FBAuth, deleteMessage);
 
 // user routes
+app.get('/user/:handle', getUserDetails)
 app.get("/user", FBAuth, getAuthenticatedUser);
 app.post("/signup", signup);
 app.post("/login", login);
@@ -50,19 +52,22 @@ app.post("/user", FBAuth, addUserDetails);
 //  https://baseurl.com/api/
 exports.api = functions.https.onRequest(app);
 
-exports.createNotificationsOnLike = functions.firestore.document("/likes/{id}")
+exports.createNotificationOnLike = functions.firestore
+    .document("/likes/{id}")
     .onCreate((snapShot) => {
       db.doc(`/messages/${snapShot.data().messageId}`).get()
           .then((doc) => {
             if (doc.exists) {
-              return db.doc(`/notifications/${snapShot.id}`).set({
-                createdAt: new Date().toISOString(),
-                recipient: doc.data().userHandle,
-                sender: snapShot.data().userHandle,
-                type: "like",
-                read: false,
-                messageId: doc.id,
-              });
+              return (
+                db.doc(`/notifications/${snapShot.id}`).set({
+                  createdAt: new Date().toISOString(),
+                  recipient: doc.data().userHandle,
+                  sender: snapShot.data().userHandle,
+                  type: "like",
+                  read: false,
+                  messageId: doc.id,
+                })
+              );
             }
           })
           .then(() => {
@@ -74,8 +79,7 @@ exports.createNotificationsOnLike = functions.firestore.document("/likes/{id}")
           });
     });
 
-exports.deleteNotificationOnUnlike = functions
-    .firestore
+exports.deleteNotificationOnUnlike = functions.firestore
     .document("likes/{id}")
     .onDelete((snapShot) => {
       db.doc(`/notifications/${snapShot.id}`)
@@ -89,20 +93,22 @@ exports.deleteNotificationOnUnlike = functions
     });
 
 
-exports.createNotificationsOnComment = functions.firestore
+exports.createNotificationOnComment = functions.firestore
     .document("/comments/{id}")
     .onCreate((snapShot) => {
       db.doc(`/messages/${snapShot.data().messageId}`).get()
           .then((doc) => {
             if (doc.exists) {
-              return db.doc(`/notifications/${snapShot.id}`).set({
-                createdAt: new Date().toISOString(),
-                recipient: doc.data().userHandle,
-                sender: snapShot.data().userHandle,
-                type: "comment",
-                read: false,
-                messageId: doc.id,
-              });
+              return (
+                db.doc(`/notifications/${snapShot.id}`).set({
+                  createdAt: new Date().toISOString(),
+                  recipient: doc.data().userHandle,
+                  sender: snapShot.data().userHandle,
+                  type: "comment",
+                  read: false,
+                  messageId: doc.id,
+                })
+              );
             }
           })
           .then(() => {
