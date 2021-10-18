cd898f2b5e08f031fcb4d7e96cd7e5cedad78189
mikeyp957 <peraram2@gmail.com>
2021-10-17 22:37:27 -0700

    restarting react

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  apiKey: "AIzaSyCUXxaSrjzp-eSSTuT4M7FD947exO-4GCA",
+  authDomain: "adventure-planner-fb111.firebaseapp.com",
+  projectId: "adventure-planner-fb111",
+  storageBucket: "adventure-planner-fb111.appspot.com",
+  messagingSenderId: "700952279621",
+  appId: "1:700952279621:web:dc444bbe408ff62e02c12c",
+  measurementId: "G-YDQFRZ9WC5",
+};
