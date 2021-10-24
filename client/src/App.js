458f64ecc871854fd9f1c9918a154afd07ef5a24
mikeyp957 <peraram2@gmail.com>
2021-10-23 22:46:39 -0700

    Material UI theme provider implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/navbar';
 import home from './pages/home';
@@ -5,20 +6,43 @@ import login from './pages/login';
 import signup from './pages/signup';
 import './App.css';
 
+//MUI
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createTheme from '@material-ui/core/styles/createTheme'
+
+const theme = createTheme({
+  palette : {
+      primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff',
+    }
+  },
+})
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-      <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path ='/' component={home} />
-            <Route exact path ='/login' component={login} />
-            <Route exact path ='/signup' component={signup} />
-          </Switch>
-        </div>
-      </Router>
+    <MuiThemeProvider theme={theme}>
+    <div className="App">      
+        <Router>
+        <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path ='/' component={home} />
+              <Route exact path ='/login' component={login} />
+              <Route exact path ='/signup' component={signup} />
+            </Switch>
+          </div>
+        </Router>         
     </div>
+    </MuiThemeProvider> 
   );
 }
 
